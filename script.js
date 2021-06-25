654950a67fdcee0477e3625f46c8be87f8098393
Brian <bmessick12@gmail.com>
2021-06-24 20:09:28 -0600

    hi

---
+++ script.js
@@ -0,0 +1,10 @@
+unction loadImage() {
+//create html element
+    let source = document.getElementById("image").value;
+//retrieve user url
+    let img = document.createElement("img");
+    img.src = source;
+//put image on page
+    img.setAttribute[]
+
+}
